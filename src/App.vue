127c445b4eda719109e5dd6a8249071f799ceeeb
David Leda <davmail99@yahoo.com>
2023-05-17 15:46:28 -0300

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="main max-w-400">
+
+    <h1>Contador</h1>
+    <br>
+    <button @click="decrement">➖️</button>
+    <span>{{ counter }}</span>
+    <button @click="increment">➕️</button>
+    
+  </main>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const counter = ref<number>(0)
+const decrement = () : boolean | number => (counter.value > 0) && counter.value--
+const increment = () : number => counter.value++
+
+</script>
+
+<style scoped>
+
+h1 {
+  text-decoration: underline;
+}
+
+span {
+  margin: 0 1rem;
+  font-size: 1.8rem;
+  font-weight: bold;
+}
+
+</style>
---
+++ src/components/SomeComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+  props: {
+    someProp: String
+  },
+
+  data: () => ({ 
+    someData: ''
+  }),
+
+  computed: {
+    someComputed() {
+      return this.someData
+    }
+  },
+
+  methods: {
+    someMethod() {
+      this.someData
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
